7a9de5148f99d9454e6dcd0be9e0a833a08207a7
aaybee1234 <43236536+aaybee1234@users.noreply.github.com>
2023-02-07 17:58:38 +0500

    main

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "abubakr";
+// MySQL Database Name
+$sql_db_name = "test";
+
+// Site URL
+$site_url = "http://localhost"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "123456"; // Your purchase code, don't give it to anyone. 
+?>
